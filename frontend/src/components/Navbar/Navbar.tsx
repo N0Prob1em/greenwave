b02f24563858d39df88426e163e026e25534c603
danchou-san <arianowong@gmail.com>
2023-06-15 11:12:21 +0200

    Added navbar

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './Navbar.css';
+import Searchbar from "../Searchbar/Searchbar";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="title">GreenWave</div>
+
+        <ul>
+          <li><a className="active" href="#home">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <Searchbar/>
+      </div>
+    </>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Searchbar.css'
+
+const Searchbar = () => {
+    
+
+    return (
+        <>
+            <input type="text" />
+        </>
+    )
+}
+
+export default Searchbar;
