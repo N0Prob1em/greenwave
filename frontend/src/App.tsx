f7de4188c83eb3f99eca79a8a995869360eb120b
danchou-san <arianowong@gmail.com>
2023-06-25 23:12:16 +0200

    Added pages and global redesign

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StartPage from './components/StartPage/StartPage';
 import ProductsPage from './components/ProductsPage/ProductsPage'
+import SingleProduct from './components/SingleProduct/SingleProduct'
 import './App.css'
 
 function App() {
@@ -7,10 +9,15 @@ function App() {
   return (
     <>
     <div className='flex justify-center'>
-      <div className='laptop:w-3/4 bg-white'>
-        <Navbar/>
-        <ProductsPage/>
-      </div>
+      <div className='laptop:w-3/4 bg-white phone:w-full'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<StartPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/product/:id" element={<SingleProduct />} />
+          </Routes>
+        </BrowserRouter>
+        </div>
     </div>
     </>
   )
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,50 +2,173 @@
 import { useEffect, useRef, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/images/logo-no-background.png';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { styled } from '@mui/system';
+import React from 'react';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const StyledAppBar = styled(AppBar)`
+  background-color: #333333;
+`;
 
 const Navbar = () => {
-  const navBar = useRef<HTMLDivElement>(null);
-  const bottomNav = useRef<HTMLDivElement>(null);
-  const [sticky, setSticky] = useState(false);
-  const [offset, setOffset] = useState(0);
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   
-  useEffect(() => {
-    if (!bottomNav.current) {
-      return;
-    }
-    setOffset(bottomNav.current.offsetTop);
-  }, [bottomNav, setOffset]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!bottomNav.current) {
-        return;
-      }
-
-      setSticky(window.scrollY > offset);
-
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [setSticky, bottomNav, offset]);
-
-
-  if (sticky) {
-    bottomNav.current?.classList.add('fixed');
-    bottomNav.current?.classList.add('top-0');
-  } else {
-    bottomNav.current?.classList.remove('fixed');
-    bottomNav.current?.classList.remove('top-0');
-  }
-  console.log(sticky);
-  
-
-
-
   return (
     <>
-    <div className='w-full'>
-      <div className="top-navbar" ref={navBar}>
+    <StyledAppBar position="sticky">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <img className='h-16 py-2' src={logo} alt="" />
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+          <img className='h-16 py-4' src={logo} alt="" />
+
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </StyledAppBar>
+    {/* <div className='w-full'>
+      <div className="top-navbar">
         <div className="title">
           <img className="logo" src={logo} alt="" />
         </div>
@@ -55,14 +178,14 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="bottom-navbar w-75" ref={bottomNav}>
+      <div className="bottom-navbar">
         <button>Home</button>
         <button>Phones</button>
         <button>Food</button>
         <button>Cars</button>
         <button>Jobs</button>
       </div>
-    </div>
+    </div> */}
     </>
   )
 }
