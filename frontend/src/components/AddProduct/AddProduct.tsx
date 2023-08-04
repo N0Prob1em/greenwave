0bba0b51cdaccd9a54f230dd0cf66e685b5b6117
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-04 13:31:46 +0200

    closed tas #106 #118

--- frontend/src/components/AddProduct/AddProduct.tsx
+++ frontend/src/components/AddProduct/AddProduct.tsx
@@ -1,118 +1,98 @@
 import React, { useState, useEffect } from "react";
+import { supabase } from './superbaseConfig';
+import { v4 as uuidv4 } from "uuid";
 import PostApi from '../../api/PostApi';
 import Navbar from '../Navbar/Navbar';
 import ImageUploader from './ImageUploader';
 
 const AddProductPage: React.FC = () => {
+  const [fileData, setFileData] = useState<File | null>(null);
   const [title, setTitle] = useState("");
   const [tag, setTags] = useState<string[]>([]);
-  const [imageUrl, setImgUrl] = useState<string | null>(null);
   const [description, setDescription] = useState("");
-  const [post, setPost] = useState<IPost>({
-    title: "",
-    description: "",
-    tag: [],
-    imageUrl: "",
-    dateAdded: "",
-  });
+  const [imageUrl, setImgUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (imageUrl !== null) {
+      const post = {
+        title,
+        description,
+        tag,
+        imageUrl,
+        dateAdded: `${new Date().toISOString().split("T")[0]} ${new Date().toLocaleTimeString().split("PM")[0].trimEnd()}`,
+      };
+      submitPost(post);
+    }
+  }, [imageUrl, title, description, tag]);
+
+  const submitPost = async (post: IPost) => {
+    try {
+      const response = await PostApi.postProduct(post);
+      if (response) {
+        alert("Added successfully..");
+        setTitle("");
+        setTags([]);
+        setDescription("");
+        setFileData(null);
+        setImgUrl(null);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   interface IPost {
     title: string,
     description: string,
-    tag: {},
+    tag: string[],
     imageUrl: string | null,
-    dateAdded: string,
   }
-  console.log(imageUrl);
-  const handleFormSubmit = (e: React.FormEvent) => {
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setPost({
-      title: title,
-      description: description,
-      tag: tag,
-      imageUrl: imageUrl,
-      dateAdded: new Date().toISOString().split("T")[0] + " " + new Date().toLocaleTimeString().split("PM")[0].trimEnd()
-    });
-  };
+    if (!fileData) return;
 
-  useEffect(() => {
-    const submitPost = async () => {
-      try {
-        if (post !== null) {
-          const response = await PostApi.postProduct(post);
-          if (response !== null)
-            alert("Added successfully..");
-        }
-      } catch (error) {
-        console.log(error);
+    try {
+      const filename = `${uuidv4()}_${fileData.name}`;
+      const { data, error } = await supabase.storage.from('GreenWave').upload(filename, fileData);
+
+      if (error) {
+        console.error('Failed to upload image.', error);
+      } else if (data) {
+        const uploadedImageURL = await supabase.storage.from('GreenWave').getPublicUrl(filename);
+        setImgUrl(uploadedImageURL.data.publicUrl);
       }
-    };
-
-    if (post.title !== "") {
-      submitPost();
+    } catch (error) {
+      console.error('Error occurred while uploading the image:', error);
     }
-  }, [post]);
+  };
 
   return (
     <>
-    <Navbar/>
-    <div className="max-w-2xl mx-auto p-8 mt-8 bg-white rounded shadow">
-      <h2 className="text-2xl font-semibold mb-6">Add Product</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="mb-4">
-          <label htmlFor="title" className="block font-medium">
-            Product Name
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="mt-1 p-2 w-full border rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="tags" className="block font-medium">
-            Tags
-          </label>
-          <input
-            type="text"
-            id="tag"
-            name="tag"
-            value={tag}
-            onChange={(e) => setTags(e.target.value.split(","))}
-            className="mt-1 p-2 w-full border rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="description" className="block font-medium">
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            rows={3}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="mt-1 p-2 w-full border rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <ImageUploader setImgUrl={setImgUrl} />
-          {imageUrl && <img src={imageUrl} className="object-contain h-48 w-96 ..." alt="Uploaded" />}
-        </div>
-        
-        <div className="flex justify-end">
-          <button
-            type="submit"
-            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-gray-600"
-          >
-            Add Product
-          </button>
-        </div>
-      </form>
-    </div>
+      <Navbar />
+      <div className="max-w-2xl mx-auto p-8 mt-8 bg-white rounded shadow">
+        <h2 className="text-2xl font-semibold mb-6">Add Product</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-medium">Product Name</label>
+            <input type="text" id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} className="mt-1 p-2 w-full border rounded" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="tags" className="block font-medium">Tags</label>
+            <input type="text" id="tag" name="tag" value={tag} onChange={(e) => setTags(e.target.value.split(","))} className="mt-1 p-2 w-full border rounded" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-medium">Description</label>
+            <textarea id="description" name="description" rows={3} value={description} onChange={(e) => setDescription(e.target.value)} className="mt-1 p-2 w-full border rounded" />
+          </div>
+          <div className="mb-4">
+            <ImageUploader setFileData={setFileData} />
+          </div>
+          <div className="flex justify-end">
+            <button type="submit" className="bg-green-600 text-white px-4 py-2 rounded hover:bg-gray-600">Add Product</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
--- frontend/src/components/AddProduct/imageUploader.tsx
+++ frontend/src/components/AddProduct/imageUploader.tsx
@@ -1,45 +1,28 @@
 import React, { ChangeEvent, useState } from 'react';
-import { supabase } from './superbaseConfig';
 
 interface ImageUploadProps {
-    setImgUrl: React.Dispatch<React.SetStateAction<string | null>>;
-  }
+    setFileData: React.Dispatch<React.SetStateAction<File | null>>;
+}
 
-const ImageUpload: React.FC<ImageUploadProps> = ({ setImgUrl }) => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+const ImageUpload: React.FC<ImageUploadProps> = ({ setFileData }) => {
+  const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>(null);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      setSelectedFile(file);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!selectedFile) return;
-
-    const fileData = selectedFile;
-    const fileName = selectedFile.name;
-
-    try {
-      const { data, error } = await supabase.storage.from('GreenWave').upload(fileName, fileData);
-      const url = await supabase.storage.from('GreenWave').getPublicUrl(fileName);
-
-      if (error) {
-        console.error('Failed to upload image.', error);
-      } else if (data) {
-        const uploadedImageURL = await supabase.storage.from('GreenWave').getPublicUrl(fileName);
-        setImgUrl(uploadedImageURL.data.publicUrl);
-      }
-    } catch (error) {
-      console.error('Error occurred while uploading the image:', error);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+        setFileData(file);
     }
   };
 
   return (
     <div>
       <input type="file" className="border rounded p-2" onChange={handleFileChange} />
-      <button className="bg-gray-600 text-white px-4 py-2.5 rounded hover:font-bold" onClick={handleUpload}>Upload Image</button>
+      {previewUrl && <img src={previewUrl as string} className="object-contain h-48 w-96 ..." alt="Uploaded" />}
     </div> 
     
   );
