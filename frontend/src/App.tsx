e698eaabb99ada8df64df74f6b7ccc34fb20b514
bulaigeorge <bulaigeorge@protonmail.com>
2023-08-07 13:46:05 +0200

    add product logic for user status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import SingleProduct from './components/SingleProduct/SingleProduct'
 import AddProduct from './components/AddProduct/AddProduct';
 import './App.css'
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from './components/Authentication/LoginButton';
 
 function App() {
 
@@ -13,13 +14,14 @@ function App() {
   return (
     <>
     <div className='flex justify-center'>
-      <div className='laptop:w-3/4 bg-white phone:w-full'>
+      <div className='laptop:w-3/4 bg-white phone:w-full max-w-7xl'>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<StartPage />} />
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/product/:id" element={<SingleProduct />} />
-            {isAuthenticated && (<Route path="/add" element={<AddProduct />}/>)}
+            {/* {isAuthenticated && (<Route path="/add" element={<AddProduct />}/>)} */}
+            {isAuthenticated ? <Route path="/add" element={<AddProduct />} /> : <Route path="/add" element={<LoginButton />} />}
           </Routes>
         </BrowserRouter>
         </div>
