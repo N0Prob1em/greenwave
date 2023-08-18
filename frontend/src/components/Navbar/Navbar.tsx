1a39808f4782799040c075a32859269886b2bbbe
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-18 12:47:29 +0200

    Fixed style for Nav bar.

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-//import React from "react";
 import { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/images/logo-no-background.png';
@@ -10,61 +9,103 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { styled } from '@mui/system';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginButton from '../Authentication/LoginButton';
-import LogoutButton from '../Authentication/LogoutButton';
 import UserProfile from '../Authentication/UserProfile';
 import { useAuth0 } from "@auth0/auth0-react";
-
+import LogoutButton from '../Authentication/LogoutButton';
 
 const destination = ['', 'products', 'categories', 'about', 'add']
 const pages = ['home', 'all products', 'categories', 'about us', 'add product'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const StyledAppBar = styled(AppBar)`
   background-color: #333333;
 `;
 
 const Navbar = () => {
-
   const { isAuthenticated } = useAuth0();
 
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  
   return (
     <>
-    <StyledAppBar position="sticky">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Link to={`/`}>
+      <StyledAppBar position="sticky">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Link to={`/`}>
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                <img className='h-16 py-2' src={logo} alt="" />
+              </Typography>
+            </Link>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {destination.map((dest, i) => (
+                  <MenuItem key={dest} onClick={handleCloseNavMenu}>
+                    <Link to={`/${dest}`}><Typography textAlign="center">{pages[i]}</Typography></Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
             <Typography
-              variant="h6"
+              variant="h5"
               noWrap
+              component="a"
+              href=""
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
                 fontFamily: 'monospace',
                 fontWeight: 700,
                 letterSpacing: '.3rem',
@@ -72,122 +113,32 @@ const Navbar = () => {
                 textDecoration: 'none',
               }}
             >
-              <img className='h-16 py-2' src={logo} alt="" />
+              <img className='h-16 py-4' src={logo} alt="" />
+
             </Typography>
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
               {destination.map((dest, i) => (
-                <MenuItem key={dest} onClick={handleCloseNavMenu}>
-                  <Link to={`/${dest}`}><Typography textAlign="center">{pages[i]}</Typography></Link>
-                </MenuItem>
+                <Link to={`/${dest}`} key={dest}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {pages[i]}
+                  </Button>
+                </Link>
               ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          <img className='h-16 py-4' src={logo} alt="" />
-
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {destination.map((dest, i) => (
-              <Link to={`/${dest}`} key={dest}>
-              <Button
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {pages[i]}
-              </Button>
-              </Link>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="green-wave-website-favicon-white.png" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <div>
-            {isAuthenticated && (
-              <>
-                <LogoutButton/>
-                <UserProfile/>
-              </>
-            )}
-            {!isAuthenticated && (
-              <>
-                <LoginButton/>
-              </>
-            )}
-          </div>
-        </Toolbar>
-      </Container>
-    </StyledAppBar>
+            </Box>
+            <div>
+              {isAuthenticated && (<UserProfile />)}
+              {!isAuthenticated && (
+                <>
+                  <LoginButton />
+                </>
+              )}
+            </div>
+          </Toolbar>
+        </Container>
+      </StyledAppBar>
     </>
   )
 }
