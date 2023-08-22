aaa6761906ba725978e4418d7b1798e59a30aa10
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-22 13:31:00 +0200

    Created About us section. closed #157

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import StartPage from './components/StartPage/StartPage';
 import ProductsPage from './components/ProductsPage/ProductsPage'
 import SingleProduct from './components/SingleProduct/SingleProduct'
 import AddProduct from './components/AddProduct/AddProduct';
+import AboutUsPage from './components/AboutUs/AboutUsPage';
 import ContactUsForm from './components/ContactUs/ContactUsForm';
 import './App.css'
 import { useAuth0 } from "@auth0/auth0-react";
@@ -23,6 +24,7 @@ function App() {
             <Route path="/" element={<StartPage />} />
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/product/:id" element={<SingleProduct />} />
+            <Route path="/about" element={<AboutUsPage />} />
             {isAuthenticated ? <Route path="/add" element={<AddProduct />} /> : <Route path="/add" element={<LoginButton />} />}
             {isAuthenticated ? <Route path="/contactus" element={<ContactUsForm />} /> : <Route path="/contactus" element={<LoginButton />} />}
           </Routes>
