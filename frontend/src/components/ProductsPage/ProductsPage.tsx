096a733aa819b8f274d868e61e9830bc50347c04
bulaigeorge <bulaigeorge@protonmail.com>
2023-08-23 14:46:37 +0200

    clean version, a lot of refactoring.

--- frontend/src/components/ProductsPage/ProductsPage.tsx
+++ frontend/src/components/ProductsPage/ProductsPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import ProductsApi from '../../api/PostApi';
 import ProductCard from './ProductCard/ProductCard';
-import './ProductsPage.css';
 
 export interface IProduct {
   id: string;
