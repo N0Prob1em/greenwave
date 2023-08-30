cb36ff56b0951d42f735b2b0911fda2dfaf2e9b9
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-30 11:25:32 +0200

    Fixed product Link For random product view.

--- frontend/src/components/StartPage/StartPage.tsx
+++ frontend/src/components/StartPage/StartPage.tsx
@@ -4,6 +4,7 @@ import ProductCard from '../ProductsPage/ProductCard/ProductCard';
 import { Button } from '@mui/material';
 import { styled } from '@mui/system';
 import { Link } from 'react-router-dom';
+import { useProduct } from '../ProductsPage/ProductContext';
 
 export interface IProduct {
   id: string;
@@ -25,6 +26,11 @@ const StartPage = () => {
   const initialProducts: IProduct[] = []
   const [products , setProducts] = useState(initialProducts);
   const [randomInt, setRandomInt] = useState(0);
+  const { setProductId } = useProduct();
+
+  const handleProductClick = (productId: string) => {
+    setProductId(productId); 
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,18 +58,18 @@ const StartPage = () => {
       <div className="flex flex-col">
         <div className="flex bg-[#383434] mt-10">
           <div className='w-1/3'>
-            <Link to={'/product/' + randomProduct?.id}>
+            <Link to="/product" onClick={() => handleProductClick(randomProduct?.id)}>
               <img className="object-cover h-96 w-full" src={randomProduct?.imageUrl} alt="" />
             </Link>
           </div>
           <div className='w-2/3 text-white p-8'>
-            <Link to={'/product/' + randomProduct?.id}>
+            <Link to="/product" onClick={() => handleProductClick(randomProduct?.id)}>
               <p className='text-5xl font-bold'>{randomProduct?.title}</p>
             </Link>
             <br />
             <p>{randomProduct?.description}</p>
             <br />
-            <Link to={'/product/' + randomProduct?.id}><ViewButton>View</ViewButton></Link>
+            <Link to="/product" onClick={() => handleProductClick(randomProduct?.id)}><ViewButton>View</ViewButton></Link>
           </div>
 
         </div>
