dd2df83a2bb8906891396f122de91946dd27f60f
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-30 11:27:33 +0200

    Used Context product id in Single product page.

--- frontend/src/components/SingleProduct/SingleProduct.tsx
+++ frontend/src/components/SingleProduct/SingleProduct.tsx
@@ -1,24 +1,28 @@
+import { Button } from '@mui/material';
+import { styled } from '@mui/system';
 import { useEffect, useState } from "react";
 import ProductsApi from '../../api/PostApi';
+import { IProduct } from "../ProductsPage/ProductsPage";
+import { useProduct } from "../ProductsPage/ProductContext";
 
-export interface IProduct {
-  id: string;
-  title: string;
-  description: string;
-  imageUrl: string
-  dateAdded: string
-}
+const ViewButton = styled(Button)`
+  background-color: #7FC37E;
+  color: white;
+  &:hover {
+    background-color: #6a9f66;
+  }
+`;
 
 const SingleProduct = () => {
   const [product, setProduct] = useState<IProduct | null>(null);
+  const { productId } = useProduct();
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const url = window.location.href;
-      const uuid = url.substring(url.lastIndexOf('/') + 1);
-
-      const response = await ProductsApi.getProductById(uuid);
+      if (productId !== null) {
+      const response = await ProductsApi.getProductById(productId);
       setProduct(response.data);
+      }
     }
     fetchProduct();
 
@@ -26,26 +30,26 @@ const SingleProduct = () => {
 
   return (
     <>
-      {product && (
-        <div className="flex p-6 justify-center">
-          <div className="laptop:w-1/2 flex justify-center">
-            <div className="flex align-center w-full h-full mr-4">
-              <img
-                className='pic-1 rounded-t object-contain'
-                src={product?.imageUrl}
-                alt=""
-              />
-            </div>
+      <div className="flex p-6 justify-center">
+        <div className="laptop:w-1/2 flex justify-center">
+          <div className="flex align-center w-full h-full mr-4 bg-neutral-200">
+            <img
+              className='pic-1 rounded-t object-contain'
+              src={product?.imageUrl}
+              alt=""
+            />
           </div>
+        </div>
 
-          <div className="laptop:w-1/2">
-            <p className="text-3xl font-bold">{product?.title}</p>
-            <p>Posted by <b>Ariano</b></p>
-            <br />
-            <p>{product?.description}</p>
-            <br />
-          </div>
-        </div>)}
+        <div className="laptop:w-1/2">
+          <p className="text-3xl font-bold">{product?.title}</p>
+          <p>Posted by <b>Ariano</b></p>
+          <br />
+          <p>{product?.description}</p>
+          <br />
+          <ViewButton>Contact now</ViewButton>
+        </div>
+      </div>
     </>
   )
 };
