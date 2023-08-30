21a07f8b87c509b21b30ed302d9ef919526eb470
bulaigeorge <49291787+bulaigeorge@users.noreply.github.com>
2023-08-30 14:12:22 +0200

    Update Api.ts

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://gwave.azurewebsites.net'; 
-const BASE_URL = 'http://localhost:8080'; 
+const BASE_URL = 'https://gwave.up.railway.app'; 
+//const BASE_URL = 'http://localhost:8080'; 
 
 const Api = axios.create({
   baseURL: BASE_URL
