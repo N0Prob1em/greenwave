80a2ef877f681ec2d8f86f3f054dbd081ad57ce5
snehalkdm2191 <snehalkdm2191@gmail.com>
2023-08-30 11:24:36 +0200

    Changed route path for single product page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import AddProduct from './components/AddProduct/AddProduct';
 import AboutUsPage from './components/AboutUs/AboutUsPage';
 import './App.css'
 import LogoutButton from './components/Authentication/LogoutButton';
+import { ProductProvider } from './components/ProductsPage/ProductContext';
 
 function App() {
 
@@ -15,17 +16,19 @@ function App() {
     <>
     <div className='flex justify-center'>
       <div className='laptop:w-3/4 bg-white phone:w-full max-w-7xl'>
+      <ProductProvider>
         <BrowserRouter>
         <Navbar />
           <Routes>
             <Route path="/" element={<StartPage />} />
             <Route path="/products" element={<ProductsPage />} />
-            <Route path="/product/:id" element={<SingleProduct />}/>
+            <Route path="/product" element={<SingleProduct />}/>
             <Route path="/about" element={<AboutUsPage />} />
             <Route path="/add" element={<AddProduct />} />
             <Route path="/logout" element={<LogoutButton />} />
           </Routes>
         </BrowserRouter>
+        </ProductProvider>
         </div>
     </div>
     </>
